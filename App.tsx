8d88aa15b920f0f5ab796ca551e2e271d0e266bb
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2022-07-31 14:18:50 +0530

    Created interface, styles, button, dark/light mode

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch, SafeAreaView } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { myColors } from './src/styles/Colors';
+import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
+
+  const [theme, setTheme] = useState('dark');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme==='light'?styles.container: [styles.container, {backgroundColor: myColors.dark}]}>
+        <StatusBar style="auto" />
+        <Switch 
+          value={theme==='light'}
+          onValueChange={()=>setTheme(theme==='light'?'dark':'light')}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 32,
+    justifyContent: 'flex-start',
   },
 });
